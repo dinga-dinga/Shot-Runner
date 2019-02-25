4478f42b22c268c13211b97dfc9b459da4ca9d59
Unknown <yotamamir96@gmail.com>
2019-02-25 21:20:01 +0200

    Added minimap using another camera and layers

---
+++ New Unity Project/Assets/_Complete-Game/Scripts/GeneralMover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneralMover : MonoBehaviour
+{
+    public Vector3 speed;
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = startPosition + Time.time * speed;
+    }
+}
